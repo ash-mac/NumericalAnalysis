48aa6093a6e497f09fa31e94c604b1d407466df0
Ashish Sharma <2019mcb1213@iitrpr.ac.in>
2021-04-01 21:03:53 +0530

    Added choice from epsilon/num of iterations

--- bisection.m
+++ bisection.m
@@ -3,24 +3,50 @@ clear all
 f = inline('x^(-3)- 5*x+ 1');
 a0 = input('Enter a0:');
 b0 = input('Enter b0:');
-i = 1;
-if(f(a0)*f(b0)<0)
-    c0 = (a0 + b0)/2;
-else
-    disp('enter a decent interval for IVT to be valid');
-end
-c_old = 10;
-diff = abs(c0-c_old);
-while abs(diff) > 10^-4
-    a1(i,1) = a0;b1(i,1)=b0;c1(i,1) = c0;iter(i,1)=i;d(i,1) = diff;
-    if(f(a0) * f(c0)>0)
-        a0 = c0;
+while true
+    if(f(a0)*f(b0)<0)
+        c0 = (a0 + b0)/2;
+        break;
     else
-        b0 = c0;
+        disp('enter an interval for IVT to be valid');
     end
-    c_old = c0;
-    c0 = (a0 + b0)/2;
-    diff = c0-c_old;
-    i = i + 1;
 end
-T = table(iter,a1,b1,c1,d)
+choice = input('Enter 1 for computing by bounds on epsilon, \nor\n 2 for computing by number of iterations.\n');
+if(choice == 1)
+    eps = input('Enter value of epsilon : ');
+    c_old = b0;
+    diff = abs(c0-c_old);
+    i = 1;
+    while abs(diff) > eps
+        iteration(i,1)=i;a1(i,1) = a0;b1(i,1)=b0;c1(i,1) = c0;d(i,1) = diff;
+        if(f(a0) * f(c0)>0)
+            a0 = c0;
+        else
+            b0 = c0;
+        end
+        c_old = c0;
+        c0 = (a0 + b0)/2;
+        diff = c0-c_old;
+        i = i + 1;
+    end
+    T = table(iteration,a1,b1,c1,d)
+elseif(choice == 2)
+    num_iter = input('Enter number of iterations : ');
+    c_old = b0;
+    diff = abd(c0-c_old);
+    num = 0;
+    while num<num_iter
+        num = num + 1;
+        iteration(num,1) =num; a1(num,1) = a0; b1(num,1) = b0;c1(num,1) = c0;d(num,1) = diff;
+        if(f(a0) * f(c0)<0)
+            a0 = c0;
+        else
+            b0 = c0;
+        end
+        c_old = c0;
+        c0 = (a0 + b0)/2;
+        diff = c0-c_old;
+    end
+    T = table(iteration,a1,b1,c1,d)
+end
+    
